c781958820e7c61536bd26aa62949fcc55c19222
Patrícia Cavalcante <patriciacavalcante@gmail.com>
2022-08-25 17:31:15 -0300

    Calculadora com C#

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+    {
+      Soma();
+    }
+      static void Soma() 
+      {
+      Console.Clear();
+      
+      Console.WriteLine("Primeiro valor: ");
+      float v1 = float.Parse(Console.ReadLine());
+
+      Console.WriteLine(v1);
+      }
+    }
+}
