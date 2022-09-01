4e58d29def024a41983bd17083cb9a3a21979eda
Patrícia Cavalcante <patriciacavalcante@gmail.com>
2022-09-01 15:32:07 -0300

    menu e funções

--- Program.cs
+++ Program.cs
@@ -5,7 +5,29 @@
         static void Main(string[] args)
     {
  
-      Multiplicacao();
+      Menu();
+    }
+    static void Menu()
+    {
+      Console.Clear();
+      Console.WriteLine("O que gostaria de fazer?");
+      Console.WriteLine("1 - Soma");
+      Console.WriteLine("2 - Subtração");
+      Console.WriteLine("3 - Divisão");
+      Console.WriteLine("4 - Multiplicação");
+
+      Console.WriteLine("--------------------");
+      Console.WriteLine("Selecione uma opção: ");
+      short res = short.Parse(Console.ReadLine());
+      switch(res) {
+        case 1: Soma(); break;
+        case 2: Subtracao(); break;
+        case 3: Divisao(); break;
+        case 4: Multiplicacao(); break;
+        default: Menu(); break;
+
+
+      }
     }
       static void Soma()
     {
@@ -20,9 +42,9 @@
       Console.WriteLine("");
 
       float resultado = v1 + v2;
-      Console.WriteLine("O resultado da soma é " + resultado);
-      Console.WriteLine($"O resultado da soma é + {resultado}");
+      Console.WriteLine($"O resultado da soma é  {resultado}");
       Console.ReadKey();
+      Menu();
     }
       static void Subtracao()
       {
@@ -39,6 +61,7 @@
       float resultado = v1 - v2;
       Console.WriteLine($"O resultado da subtração é  {resultado}"); //interpolação
       Console.ReadKey(); //para n sair da tela
+      Menu();
     }
       static void Divisao(){
         Console.Clear();
@@ -52,7 +75,8 @@
       Console.WriteLine("");
       float resultado = v1 / v2;
       Console.WriteLine($"O resultado da divisão é {resultado}");
-      Console.ReadKey(); 
+      Console.ReadKey();
+      Menu();
 
 
     }
@@ -69,6 +93,7 @@
       float resultado = v1 * v2;
       Console.WriteLine("O resultado da multiplicação é " + (v1 * v2));
       Console.ReadKey();
+      Menu();
     }
   }
 
